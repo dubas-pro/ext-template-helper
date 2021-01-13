b85c775aa7aa509c0cb7e23d32dad91f33ca98f7
emillod <emil@dubielecki.pl>
2021-01-13 11:35:43 +0100

    dubasTranslate helper

---
+++ files/application/Espo/Modules/DubasTemplateHelper/TemplateHelpers/DubasHelper.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Espo\Modules\DubasTemplateHelper\TemplateHelpers;
+
+use LightnCandy\LightnCandy as LightnCandy;
+
+class DubasHelper
+{
+    public static function dubasTranslate()
+    {
+        $args = func_get_args();
+        $context = $args[count($args) - 1];
+        $hash = $context['hash'];
+        $data = $context['data']['root'];
+
+        $scope     = $hash['scope'] ?? null;
+        $category  = $hash['category'] ?? null;
+        $label     = $hash['label'] ?? null;
+        $language  = $hash['language'] ?? 'en_US';
+
+        if 
+        (
+            !$scope
+            ||
+            !$category
+            ||
+            !$label
+        )
+        {
+            $html = null;
+        }
+        else
+        {
+            $translation = $data['__language'];
+            $translation->setLanguage($language);
+            $html = $translation->translate($label, $category, $scope);
+        }
+
+        return new LightnCandy\SafeString($html);
+    }
+}
