05eb74e4eabedae39821949306ba23160410685c
emillod <emil@dubielecki.pl>
2021-01-13 11:43:19 +0100

    dubasTranslateOption helper

--- files/application/Espo/Modules/DubasTemplateHelper/TemplateHelpers/DubasHelper.php
+++ files/application/Espo/Modules/DubasTemplateHelper/TemplateHelpers/DubasHelper.php
@@ -38,4 +38,37 @@ class DubasHelper
 
         return new LightnCandy\SafeString($html);
     }
+
+    public static function dubasTranslateOption()
+    {
+        $args = func_get_args();
+        $context = $args[count($args) - 1];
+        $hash = $context['hash'];
+        $data = $context['data']['root'];
+
+        $scope     = $hash['scope'] ?? null;
+        $field     = $hash['field'] ?? null;
+        $value     = $hash['value'] ?? null;
+        $language  = $hash['language'] ?? 'en_US';
+
+        if 
+        (
+            !$scope
+            ||
+            !$field
+            ||
+            !$value
+        )
+        {
+            $html = null;
+        }
+        else
+        {
+            $translation = $data['__language'];
+            $translation->setLanguage($language);
+            $html = $translation->translateOption($value, $field, $scope);
+        }
+
+        return new LightnCandy\SafeString($html);
+    }
 }
