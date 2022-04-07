d5679a3fb2b9256a53095351d1126d0f5ef8acd8
Emil <emil@dubielecki.pl>
2022-04-07 22:16:32 +0200

    ecs

--- src/files/custom/Espo/Modules/DubasTemplateHelper/TemplateHelpers/DubasSpellAmount.php
+++ src/files/custom/Espo/Modules/DubasTemplateHelper/TemplateHelpers/DubasSpellAmount.php
@@ -1,5 +1,25 @@
 <?php
 
+/*
+ * This file is part of the Dubas Template Helper - EspoCRM extension.
+ *
+ * DUBAS - contact@dubas.pro
+ * Copyright (C) 2022
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 namespace Espo\Modules\DubasTemplateHelper\TemplateHelpers;
 
 use Espo\Core\Htmlizer\{
@@ -12,13 +32,13 @@ class DubasSpellAmount implements Helper
 {
     public function render(Data $data): Result
     {
-        $value      = $data->getArgumentList()[0] ?? null;
-        $number     = number_format($value, 2, '.', '');
-        $language   = $data->getOption('language') ?? 'en_US';
+        $value = $data->getArgumentList()[0] ?? null;
+        $number = number_format($value, 2, '.', '');
+        $language = $data->getOption('language') ?? 'en_US';
 
-        $dollars    = $data->getOption('dollars') ?? 'dollars';
-        $cents      = $data->getOption('cents') ?? 'cents';
-        $and        = $data->getOption('and') ?? 'and';
+        $dollars = $data->getOption('dollars') ?? 'dollars';
+        $cents = $data->getOption('cents') ?? 'cents';
+        $and = $data->getOption('and') ?? 'and';
 
         $a = bcmul($number, 100);
         $c = $a % 100;
--- src/files/custom/Espo/Modules/DubasTemplateHelper/TemplateHelpers/DubasTranslateValue.php
+++ src/files/custom/Espo/Modules/DubasTemplateHelper/TemplateHelpers/DubasTranslateValue.php
@@ -1,5 +1,25 @@
 <?php
 
+/*
+ * This file is part of the Dubas Template Helper - EspoCRM extension.
+ *
+ * DUBAS - contact@dubas.pro
+ * Copyright (C) 2022
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 namespace Espo\Modules\DubasTemplateHelper\TemplateHelpers;
 
 use Espo\Core\Htmlizer\{
@@ -24,12 +44,12 @@ class DubasTranslateValue implements Helper
 
     public function render(Data $data): Result
     {
-        $id         = $data->getArgumentList()[0] ?? null;
-        $scope      = $data->getOption('scope') ?? null;
-        $field      = $data->getOption('field') ?? null;
-        $language   = $data->getOption('language') ?? 'en_US';
+        $id = $data->getArgumentList()[0] ?? null;
+        $scope = $data->getOption('scope') ?? null;
+        $field = $data->getOption('field') ?? null;
+        $language = $data->getOption('language') ?? 'en_US';
 
-        if ( !$scope || !$category || !$label ) {
+        if (!$scope || !$category || !$label) {
             $html = null;
         } else {
             $entity = $this->entityManager->getEntity($scope, $id);
