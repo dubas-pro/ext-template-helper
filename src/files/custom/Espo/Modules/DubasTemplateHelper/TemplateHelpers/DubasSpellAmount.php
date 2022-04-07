d40d832617316f92536452ca438bf023c45e192a
Emil <emil@dubielecki.pl>
2022-04-07 22:22:48 +0200

    Adjust spell amount template helper

--- src/files/custom/Espo/Modules/DubasTemplateHelper/TemplateHelpers/DubasSpellAmount.php
+++ src/files/custom/Espo/Modules/DubasTemplateHelper/TemplateHelpers/DubasSpellAmount.php
@@ -32,24 +32,20 @@ class DubasSpellAmount implements Helper
 {
     public function render(Data $data): Result
     {
-        $value = $data->getArgumentList()[0] ?? null;
-        $number = number_format($value, 2, '.', '');
-        $language = $data->getOption('language') ?? 'en_US';
+        $value      = $data->getArgumentList()[0] ?? null;
+        $number     = number_format($value, 2, '.', '');
+        $language   = $data->getOption('language') ?? 'en_US';
 
-        $dollars = $data->getOption('dollars') ?? 'dollars';
-        $cents = $data->getOption('cents') ?? 'cents';
-        $and = $data->getOption('and') ?? 'and';
+        $dollars    = $data->getOption('dollars') ?? 'dollars';
+        $cents      = $data->getOption('cents') ?? 'cents';
+        $and        = $data->getOption('and') ?? 'and';
 
         $a = bcmul($number, 100);
         $c = $a % 100;
         $d = ($a - $c) / 100;
 
-        $amount = new NumberFormatter($language, NumberFormatter::SPELLOUT);
+        $amount = new \NumberFormatter($language, \NumberFormatter::SPELLOUT);
 
-        // print_r(sprintf("%s $dollars $and %s $cents", $amount->format($d), $amount->format($c)));
-
-
-        //return Result::createSafeString($html);
-        return Result::createSafeString(print_r(sprintf("%s $dollars $and %s $cents", $amount->format($d), $amount->format($c))));
+        return Result::createSafeString(sprintf("%s $dollars $and %s $cents", $amount->format($d), $amount->format($c)));
     }
 }
